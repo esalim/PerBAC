55529f18e38a713e696f99aad96337a4d7b440c4
AbdramCoulby <48120052+AbdramCoulby@users.noreply.github.com>
2019-07-28 22:17:25 +0100

    Changement 1

---
+++ hardware/raspberry pi/serveur.py
@@ -0,0 +1,23 @@
+﻿import socket
+
+host = ""
+port = 5005
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((host, port))
+s.listen(1)
+print("Attente d'une connexion...")
+
+(conn, addr) = s.accept()
+print("La connexion a été etablie avec succés au serveur")
+
+print("Envoi du fichier de MIT Parking en cours...")
+filename = "MIT-ParkingPlace.txt"
+
+#print("Envoi du fichier de MIT Academy Parking en cours...")
+#filename = "MIT-Academy-ParkingPlace.txt"
+
+file = open(filename,'rb')
+file_data = file.read(1024)
+conn.send(file_data)
+print("Fichier transferé avec succès vers le serveur local ! ")
---
+++ hardware/raspberry pi/thingspeakdata.py
@@ -0,0 +1,41 @@
+﻿
+import sys
+import urllib3
+import serial
+import urllib.request
+from time import sleep
+import requests
+from bs4 import BeautifulSoup
+import threading
+
+def Raspi_data():
+    port = serial.Serial("/dev/ttyACM0")
+    port.flushInput()
+    data = port.readline()
+    datadec = int(data.decode("utf-8"))
+    return (datadec)
+
+def Raspi_dataSend1():
+    ParkingPlaces = Raspi_data()
+    threading.Timer(15, Raspi_dataSend).start
+    # Envoi données ThingSpeak
+if isinstance(ParkingPlaces, int):
+    url = 'https://api.thingspeak.com/update?api_key='
+    key = '57ZNS71HXS2R8FLT'
+    header = '&field1={}'.format(ParkingPlaces)
+    link = url + key + header
+    datasend = urllib.request.urlopen(link)
+
+def Raspi_dataSend2():
+    AcademyPlaces = Raspi_data()
+    threading.Timer(15, Raspi_dataSend).start
+    # Envoi données ThingSpeak
+if isinstance(AcademyPlaces, int):
+    url = 'https://api.thingspeak.com/update?api_key='
+    key = '57ZNS71HXS2R8FLT'
+    header = '&field2={}'.format(AcademyPlaces)
+    link = url + key + header
+    datasend = urllib.request.urlopen(link)
+
+if __name__ == '__main__':
+    Raspi_dataSend1()
