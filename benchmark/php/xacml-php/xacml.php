0a1723ef70632b61d4db862b4479269379c08566
AbdramCoulby <48120052+AbdramCoulby@users.noreply.github.com>
2019-07-29 16:18:54 +0100

    Update xacml.php

--- benchmark/php/xacml-php/xacml.php
+++ benchmark/php/xacml-php/xacml.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Controle d'acces XACML</title>
+    <title>XACML Control Access</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -27,7 +27,7 @@
         <a href="../php-abac/description.php">ABAC</a>
         <a href="../PerBAC/description.php">PerBAC</a>
         <a class="active" href="description.php">XACML</a>
-        <a href="../resultats.php">Comparatifs & Resultats</a>
+        <a href="../resultats.php">Comparative & Results</a>
 
 
         <div class="dropdown">
@@ -39,7 +39,7 @@
                 <a href="../../php/enregistrement.php">Sign Up</a>
             </div>
         </div>
-        <a href="../../php/index.php" id="logout">Sortir</a>
+        <a href="../../php/index.php" id="logout">Logout</a>
     </div>
 </nav>
 <!-- The sidebar -->
@@ -57,11 +57,10 @@
                 <input type="text" name="role" class="form-control" placeholder="Role" required="required">
             </div>
             <div class="form-group">
-                <input type="text" name="place" class="form-control" placeholder="Place recherchée" required="required">
+                <input type="text" name="place" class="form-control" placeholder="Place" required="required">
             </div>
             <div class="form-group">
-                <button id="buttonV" name="connect_button" type="submit" class="btn btn-primary btn-block">Vérifier
-                    disponibilité
+                <button id="buttonV" name="connect_button" type="submit" class="btn btn-primary btn-block">Check availability
                 </button>
             </div>
 
@@ -160,7 +159,7 @@
 
 
             if (isset($_POST['connect_button'])) { // Button cliqué
-
+                $start = microtime(true);
                 $fonct = $_POST['role'];  // Envoie données par la Method POST
                 $place = $_POST['place'];
 
@@ -198,28 +197,30 @@
                     $getEtat = mysqli_fetch_assoc(mysqli_query($conn,"SELECT etat FROM dispo WHERE place = '$place'"));
                     $Dispo = $getEtat['etat'];   // recuperation etat de la place recherchée dans la Database
                     if ($Dispo===$check){   // Place dispo
-                        echo '<script type="text/javascript"> 
+                        $end = microtime(true);
+                        $responseTime = 1000*($end - $start);
+                        echo "<script type='text/javascript'> 
                     Swal.fire ({
-                    title:"Notification de disponibilité",
-                    text: "La place récherchée est libre !",
-                    imageUrl: " ../../images/pass.jpg",
+                    title: 'Availability Notification',
+                    text: \"The place sought is free and the response time : $responseTime ms\",
+                    imageUrl: \"../../images/pass.jpg\",
                     imageWidth: 1500,
                     imageHeight: 200,
-                    imageAlt: "Pass image",
-                    buttons: {confirm: "Compris" }
+                    imageAlt: \"Pass image\",
+                    buttons: {confirm: \"Compris\" }
                     }).then(val => {
                     if(val)  {
-                    Swal.fire ({ 
-                    type:"success",
-                    title: "Merci pour votre confiance...!",
-                    text: "Excellente journée",
+                    Swal.fire ({
+                    type:'success',
+                    title: 'Thank you for your trust...!',
+                    text: 'Have a nice day',
                     });}});
-                    </script>';
-                    } else { // place occupée
-                        echo '<script type="text/javascript"> 
+                    </script>";
+                    } else {  // place occupée
+                            echo '<script type="text/javascript"> 
                     Swal.fire({
-                    title: \'Notification de disponibilité\',
-                    text:"La place récherchée est déjà occupée",
+                    title: \'Availability Notification\',
+                    text:"The place sought is already occupied",
                     imageUrl: " ../../images/verif.jpg",
                     imageWidth: 1500,
                     imageHeight: 200,
@@ -227,13 +228,13 @@
                     animation: false,
                      customClass: { popup: \'animated tada\'}
                     })</script>';
-                    }
+                        }
 
                 } else { // message d'erreur
                     echo '<script type="text/javascript"> 
                     Swal.fire({
                     title: "Oops...!",
-                    html:"Vous n\'avez pas les droits d\'accès à ce parking ! </br>  Veuillez utiliser le second parking reservé à ce effet. </br>  (Voir Section Dashboard) ",
+                    html:"You do not have access rights to this car park ! </br>  Please use the second parking reserved for this purpose.</br>",
                     type:"info",
                     animation: false,
                      customClass: { popup: \'animated rubberBand\'}
