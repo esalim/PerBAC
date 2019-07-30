3aa4c93baf4980b8019f6186397d60a01923feae
AbdramCoulby <48120052+AbdramCoulby@users.noreply.github.com>
2019-07-30 09:11:53 +0100

    Update description.php

--- benchmark/php/zend-rbac/description.php
+++ benchmark/php/zend-rbac/description.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Controle d'acces RBAC</title>
+    <title>RBAC Access Control</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -26,7 +26,7 @@
         <a href="../php-abac/description.php">ABAC</a>
         <a href="../PerBAC/description.php">PerBAC</a>
         <a href="../xacml-php/description.php">XACML</a>
-        <a href="../resultats.php">Comparatifs & Resultats</a>
+        <a href="../resultats.php">Comparatives & Results</a>
 
 
         <div class="dropdown">
